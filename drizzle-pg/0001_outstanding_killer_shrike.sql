0f375897e8f994dc0b95cbc00a79fb43fe0d0882
anandspaces <anandvaibhaw000@gmail.com>
2026-04-30 12:53:41 +0530

    remove extra migration files

--- drizzle-pg/0001_outstanding_killer_shrike.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "consumers" ADD COLUMN "email" text;--> statement-breakpoint
-ALTER TABLE "consumers" ADD COLUMN "password_hash" text;--> statement-breakpoint
-CREATE UNIQUE INDEX "consumers_email_unique" ON "consumers" USING btree ("email");
